92bbf477a2a921f7438fe68d598ccfd30bc2c8ac
daun3046 <daun3046@swu.ac.kr>
2020-03-18 18:35:50 +0900

    2020.03.18.

--- 2775. 부녀회장이 될테야.py
+++ 2775. 부녀회장이 될테야.py
@@ -1,15 +1,15 @@
 T = int(input())
-aptList = [[0 for _ in range(14)] for _ in range(14)] #14x14
+aptList = [[0 for _ in range(15)] for _ in range(15)] #15x15
 
-for room in range(14):
+for room in range(15):
     aptList[0][room] = room + 1 #0층 채우기
 
-for floor in range(14): #1호는 무조건 1명
+for floor in range(15): #1호는 무조건 1명
     aptList[floor][0] = 1
 #print(aptList)
 
-for floor in range(1, 14): #1층부터 14층까지
-    for room in range(1, 14): #그외 호수들 채우기
+for floor in range(1, 15): #1층부터 14층까지
+    for room in range(1, 15): #그외 호수들 채우기
         aptList[floor][room] += 1
         for i in range(1, room + 1):
             aptList[floor][room] += aptList[floor-1][i]
