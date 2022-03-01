5ac5515cab570b14b67c8ac52e8171f5a4814db6
daun3046 <daun3046@swu.ac.kr>
2022-03-01 23:48:13 +0900

    2022.03.01.

---
+++ 10869. 사칙연산.py
@@ -0,0 +1,18 @@
+a, b = map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+'''
+# 메모리 미세하게 덜먹음
+A, B=input().split()
+A=int(A)
+B=int(B)
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
+'''
