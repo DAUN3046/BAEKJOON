7b8d5eb8e8ed9af22c26d32ebb294839c93c1f7d
daun3046 <daun3046@swu.ac.kr>
2022-04-14 01:54:00 +0900

    2022.04.14.

--- 1748. 수 이어 쓰기 1.py
+++ 1748. 수 이어 쓰기 1.py
@@ -1,3 +1,13 @@
+# 직접 수로 계산
+n = int(input())
+length, ans = len(str(n)), 0
+for i in range(1, length):
+    ans += i * (10**i - 10**(i - 1))
+    # 1 ~ 9, 11 ~ 99, ... 계산식 i * (10 ** i - 1 - 10 ** (i - 1) + 1)
+# 제일 큰 자릿수 남은 숫자들 계산
+ans += length * (n - 10**(length-1) + 1)
+print(ans)
+
 '''
 # 시간 초과
 n = int(input())
@@ -6,17 +16,10 @@ for i in range(1, n + 1):
     ans += len(str(i))
 print(ans)
 '''
-#### 아래로 통과 코드 ####
-# 로그 사용으로 연산 줄이기
+''' 로그 사용
 n = int(input())
-print(sum(map(lambda x: len(str(x)), range(1, n + 1))))
+sum = 0
+for i in range(1, n + 1):
+    sum = sum + int(math.log10(i)) + 1
+print(sum)
 '''
-# 직접 수로 계산
-n = int(input())
-l, ans = len(str(n)), 0
-for i in range(1, l):
-    ans += i * (10**i - 10**(i-1)) # 순서대로 1~9, 10~99 ... 자릿수 계산
-    # print("ans1:", ans)
-ans += l * (n-10**(l-1)+1)  # 제일 큰 자릿수의 나머지들을 계산
-
-print(ans)'''
